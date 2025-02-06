66fbed002512212317426f9cb5217e7e503c2c99
Mr.Resor <jakusienko@gmail.com>
2025-02-06 23:09:51 +0000

    add note bout used headers

--- src/api/whoami/index.js
+++ src/api/whoami/index.js
@@ -1,11 +1,14 @@
 const router = require('express').Router();
 
 router.get('/api/:date', (req, res) => {
+    /* 'x-forwarded-for' key is used since my solution uses reverse proxy to run on my machine, 
+        if such solution is not used by you, change it to 'host' instead 
+    */
     res.json({
-        "ipaddress": req.headers['x-forwarded-for'],
+        "ipaddress": req.headers['x-forwarded-for'], // replace with req.headers['host']
         "language": req.headers['accept-language'],
         "software": req.headers['user-agent']
-      });
+    });
 });
 
 module.exports = router;
