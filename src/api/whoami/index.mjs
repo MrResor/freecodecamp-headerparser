5bcac5424696c031289ef5e6e3fac4c3b5abbff3
Mr.Resor <jakusienko@gmail.com>
2025-05-20 21:04:16 +0000

    remove unnecessary quotes

--- src/api/whoami/index.mjs
+++ src/api/whoami/index.mjs
@@ -6,10 +6,10 @@ whoami.get('/api/:date', (req, res) => {
     /* 'x-forwarded-for' key is used since my solution uses reverse proxy to run on my machine, 
         if such solution is not used by you, change it to 'host' instead 
     */
-    res.json({
-        "ipaddress": req.headers['x-forwarded-for'], // replace with req.headers['host']
-        "language": req.headers['accept-language'],
-        "software": req.headers['user-agent']
+    res.status(200).json({
+        ipaddress: req.headers['x-forwarded-for'], // replace with req.headers['host']
+        language: req.headers['accept-language'],
+        software: req.headers['user-agent']
     });
 });
 
